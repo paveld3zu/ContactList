64d8515471b300865fea99baced5bc9bf06f11e5
paveld3zu <pavel.karunnyj@yandex.ru>
2023-04-18 10:19:50 +0300

    Add DataStore

---
+++ ContactList/Services/DataStore.swift
@@ -0,0 +1,59 @@
+//
+//  DataStore.swift
+//  ContactList
+//
+//  Created by Pavel Karunnyj on 18.04.2023.
+//
+
+class DataStore {
+    let names = [
+        "Igor",
+        "Ekaterina",
+        "Alexey",
+        "Pavel",
+        "Elena",
+        "Maxim",
+        "Gleb",
+        "Mihail",
+        "Natasha"
+    ]
+    
+    let surnames = [
+        "Petrov",
+        "Ivanov",
+        "Sidorov",
+        "Smirnov",
+        "Popov",
+        "Kuznetsov",
+        "Sokolov",
+        "Volkov",
+        "Novikov",
+        "Mikhailov"
+    ]
+    
+    let emails = [
+        "1@gmail.com",
+        "2@gmail.com",
+        "3@gmail.com",
+        "4@gmail.com",
+        "5@gmail.com",
+        "6@gmail.com",
+        "7@gmail.com",
+        "8@gmail.com",
+        "9@gmail.com",
+        "10@gmail.com"
+    ]
+    
+    let phoneNumbers = [
+        555-901-2345,
+        555-123-4567,
+        555-567-8901,
+        555-234-5678,
+        5555-567-8901,
+        555-456-7890,
+        555-345-6789,
+        555-234-5678,
+        555-012-3456,
+        555-901-2345
+    ]
+}
