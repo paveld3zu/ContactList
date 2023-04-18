7e9bfefd93db8219c1d704a635d8b63835cf5d27
paveld3zu <pavel.karunnyj@yandex.ru>
2023-04-18 13:52:32 +0300

    Add Models, second view.

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -10,4 +10,24 @@ struct Person {
     let surname: String
     let email: String
     let phoneNumber: String
+    
+    static func getPerson() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        var names = DataStore().names.shuffled()
+        var surnames = DataStore().surnames.shuffled()
+        var emails = DataStore().emails.shuffled()
+        var phoneNumbers = DataStore().phoneNumbers.shuffled()
+        
+        for _ in DataStore().names {
+            persons.append(Person(
+                name: names.remove(at: 0),
+                surname: surnames.remove(at: 0),
+                email: emails.remove(at: 0),
+                phoneNumber: phoneNumbers.remove(at: 0)
+            ))
+        }
+        return persons
+    }
 }
---
+++ ContactList/ViewControllers/ContactInfoViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ContactInfoViewController.swift
+//  ContactList
+//
+//  Created by Pavel Karunnyj on 18.04.2023.
+//
+
+import UIKit
+
+class ContactInfoViewController: UIViewController {
+    
+    @IBOutlet var phoneLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    
+    var contact: Person!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        phoneLabel.text = "Phone: \(contact.phoneNumber)"
+        emailLabel.text = "Email: \(contact.email)"
+    }
+}
