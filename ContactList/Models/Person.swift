59b3f1d9b57b90f646f3a508c35d4da83beace29
paveld3zu <pavel.karunnyj@yandex.ru>
2023-04-18 10:23:42 +0300

    Fix DataStore

---
+++ ContactList/Models/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by Pavel Karunnyj on 18.04.2023.
+//
+
+struct Person {
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+}
